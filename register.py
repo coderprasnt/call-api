9ed066c16d764c690fc18c4ce0145359af49022f
Prasant <coderprasant@gmail.com>
2025-03-14 10:00:23 +0545

    Create register.py

---
+++ register.py
@@ -0,0 +1,29 @@
+#In this code we will call our api to register a user...You can do it via web also!!
+#This is a working code... we will give you url once you reached us!
+
+import requests
+import json
+
+# API URL where the PHP file is hosted
+API_URL = ''  
+def signup():
+    data = {
+        'api_key': 'FAITHSIRG',
+        'action': 'signup',
+        'username': 'testus1er',
+        'password': 'password123',
+        'email': 'test@e1xample.com',
+        'firstname': 'Test' # Optional
+    }
+    response = requests.post(API_URL, json=data)
+    print("Raw Response:", response.text)  # Print raw response to debug
+    try:
+        print(response.json())
+    except requests.exceptions.JSONDecodeError as e:
+        print("JSON Decode Error:", e)
+
+# Example Check Secret Request
+
+# Call the function you want to test
+signup()
+
